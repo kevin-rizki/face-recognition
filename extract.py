a3d93a4fb00c835157f6c562953450d594b5d2d3
fabianzhafran <fabianzhafransyahharahap@gmail.com>
2019-10-30 12:50:06 +0700

    GUI for showing images

--- extract.py
+++ extract.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import scipy
-import random
 import os
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -15,6 +14,18 @@ def TraverseImages(root_path='./PINS'):
         print(file_list)
 
 # Image path generator
+        
+def VectorSize(vec):
+    sum = float(0)
+    for i in range(len(vec)):
+        sum = sum + vec[i] * vec[i]
+    return sum**(1/2.0)
+
+def DotProduct(vector_1, vector_2):
+    for i in range (len(vector_1)):
+        for j in range(len(vector_1[0])):
+            print('yes')
+
 def ImagePath(imgdir):
     temp = './PINS/pins_Aaron Paul/%s' %(imgdir)
     return temp
@@ -25,7 +36,7 @@ def ImageMatcher(img_path1, img_path2):
         # Load image
         img1 = cv2.imread(img_path1)
         img2 = cv2.imread(img_path2)
-        
+
         # We don't need RGB since we're only detecting a single face per image. Result is a hued grayscale image
         img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
         img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
@@ -34,19 +45,23 @@ def ImageMatcher(img_path1, img_path2):
         kaze = cv2.KAZE_create()
         keypt1, desc1 = kaze.detectAndCompute(img1_gray, None)
         keypt2, desc2 = kaze.detectAndCompute(img2_gray, None)
-        
+
         # desc1 = np.uint8(desc1)
         # desc2 = np.uint8(desc2)
         
         # bf = cv2.BFMatcher(cv2.NORM_HAMMLING)
-        bf = cv2.BFMatcher(cv2.NORM_L1)
-        matches = bf.knnMatch(desc1, desc2, k = 2)
+        # bf = cv2.BFMatcher(cv2.NORM_L1)
+        # matches = bf.knnMatch(desc1, desc2, k = 2)
         
-        good = []
-        for m, n in matches:
-            if m.distance < 0.9 * n.distance :
-                good.append([m])
+        # good = []
+        # for m, n in matches:
+        #    if m.distance < 0.9 * n.distance :
+        #        good.append([m])
+        
                 
-        return len(good)
+        return desc1
     except cv2.error as e:
         print('Error: %s' %(e))
+        
+# print(ImageMatcher('./Justin_Bieber_2010_3.jpg', './Justin_Bieber_2010_3.jpg')[0])
+    
