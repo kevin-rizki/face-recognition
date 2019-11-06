2dec83d51da3a3d408cc7587afcd0a812fa9d7f9
fabianzhafran <fabianzhafransyahharahap@gmail.com>
2019-11-06 12:25:28 +0700

    Benerin GUI, tinggal finishing touch

--- main.py
+++ main.py
@@ -3,18 +3,13 @@ from tkinter import ttk
 import tkinter as tk
 from tkinter import  *
 from PIL import ImageTk, Image
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 LARGE_FONT = ("Verdana", 16)
 
 image_path = "test"
 #fig = plt.figure(figsize=(8,5) ,dpi=100)
 
-class SeaofBTCapp(tk.Tk):
+class Window(tk.Tk):
     currentFrame = None
 
     def __init__(self, *args, **kwargs):
@@ -67,10 +62,20 @@ class PageOne(tk.Frame):
     sub2 = None
     sub3 = None
     sub4 = None
+    
     fig1 = None
     fig2 = None
     fig3 = None
     fig4 = None
+    
+    imgname2 = None
+    imgname3 = None
+    imgname4 = None
+
+    accuracy2 = None
+    accuracy3 = None
+    accuracy4 = None
+    
     buttons = None
     canvas = None
 
@@ -85,20 +90,19 @@ class PageOne(tk.Frame):
             #     key_press_handler(event, canvas, toolbar)
         
         self.buttons = tk.Frame(self)
-        self.buttons.pack(side = BOTTOM, expand = True, fill = BOTH)
+        self.buttons.grid(row = 1, column = 0, sticky = "NEWS")
 
         self.canvas = tk.Canvas(self, width=1280, height=600)
-        self.canvas.pack(side = TOP, expand = True, fill = tk.BOTH)
+        self.canvas.grid(row = 0, column = 0, sticky = "NEWS")
         
         button0 = ttk.Button(self.buttons, text="Pilih Gambar", command = lambda: self.SelectImage(controller))
-        button0.pack(side = LEFT)
+        button0.grid(row = 2, column = 0, padx = (20, 20), sticky = "E")
 
         button1 = ttk.Button(self.buttons, text="Update", command = lambda: self.draw_image())
-        button1.pack(side = LEFT)
+        button1.grid(row = 2, column = 1, padx = (20, 20), sticky = "W")
         
         button2 = ttk.Button(self.buttons, text="Match", command = lambda: controller.show_frame(StartPage))
-        button2.pack(side = LEFT)
-        
+        button2.grid(row = 2, column = 2, padx = (20, 20), sticky = "W")
 
     def draw_image(self):
         global image_path
@@ -111,7 +115,7 @@ class PageOne(tk.Frame):
         # plt.imshow(img)
         self.sub1 = ImageTk.PhotoImage(Image.open(image_path).resize((540, 540), Image.ANTIALIAS))
         if (self.fig1 is None):
-            self.fig1 = self.canvas.create_image(0, 0, anchor = NW, image = self.sub1)
+            self.fig1 = self.canvas.create_image(20, 20, anchor = NW, image = self.sub1)
         else:
             self.canvas.itemconfig(self.fig1, image = self.sub1)
         # Subplot 2nd row 1st col
@@ -119,17 +123,20 @@ class PageOne(tk.Frame):
         # plt.imshow(img)
         self.sub2 = ImageTk.PhotoImage(Image.open(image_path).resize((160, 160), Image.ANTIALIAS))
         if (self.fig2 is None):
-            self.fig2 = self.canvas.create_image(560, 0, anchor = NW, image = self.sub2)
+            self.fig2 = self.canvas.create_image(580, 40, anchor = NW, image = self.sub2)
+            self.imgname2 = self.canvas.create_text(820, 60, text = "Titit gede")
+            self.accuracy2 = self.canvas.create_text(820, 90, text = "Gede banget ahhhh")
         else:
             self.canvas.itemconfig(self.fig2, image = self.sub2)
-        
 
         # Subplot 2nd row 2nd col
         # self.sub3 = fig.add_subplot(2, 3, 5)
         # plt.imshow(img)
         self.sub3 = ImageTk.PhotoImage(Image.open(image_path).resize((160, 160), Image.ANTIALIAS))
         if (self.fig3 is None):
-            self.fig3 = self.canvas.create_image(560, 180, anchor = NW, image = self.sub3)
+            self.fig3 = self.canvas.create_image(580, 220, anchor = NW, image = self.sub3)
+            self.imgname3 = self.canvas.create_text(820, 240, text = "Titit gede")
+            self.accuracy3 = self.canvas.create_text(820, 270, text = "Gede banget ahhhh")
         else:
             self.canvas.itemconfig(self.fig3, image = self.sub3)
         
@@ -139,7 +146,9 @@ class PageOne(tk.Frame):
         # plt.imshow(img)
         self.sub4 = ImageTk.PhotoImage(Image.open(image_path).resize((160, 160), Image.ANTIALIAS))
         if (self.fig4 is None):
-            self.fig4 = self.canvas.create_image(560, 360, anchor = NW, image = self.sub4)
+            self.fig4 = self.canvas.create_image(580, 400, anchor = NW, image = self.sub4)
+            self.imgname4 = self.canvas.create_text(820, 420, text = "Titit gede")
+            self.accuracy4 = self.canvas.create_text(820, 450, text = "Gede banget ahhhh")
         else:
             self.canvas.itemconfig(self.fig4, image = self.sub4)
         
