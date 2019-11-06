9d6ddd130a7edfa09aa742fa9907fab1aab0cc1b
ManEnfu <hxrd97@gmail.com>
2019-11-06 20:06:15 +0700

    Get the matcher working

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from tkinter import ttk, filedialog
 from PIL import ImageTk, Image
 from vector import *
 from extract import *
+from threading import Thread
 
 LARGE_FONT = ("Verdana", 16)
 
-image_path = "test"
+image_path = None
 db_path = 'pins.db'
 db = None
-#fig = plt.figure(figsize=(8,5) ,dpi=100)
+t_compare = 3
 
 class MainApp(Tk):
     currentFrame = None
@@ -23,15 +24,12 @@ class MainApp(Tk):
         container = Frame(self)
         container.pack(side="top", fill="both", expand = True) 
         
-        #container.grid_rowconfigure(0, weight = 1)
-        #container.grid_columnconfigure(0, weight = 1)
         
         self.frames = {}
         for F in ([PageOne]):
             frame = F(container, self)
             self.frames[F] = frame
             frame.pack_forget()
-            #frame.pack(side="top", fill = "both", expand = True)
         
         self.show_frame(PageOne)
         self.currentFrame.load_db(db_path)
@@ -40,7 +38,7 @@ class MainApp(Tk):
         if self.currentFrame is not None:
             self.currentFrame.pack_forget()
         self.currentFrame = self.frames[cont]
-        self.currentFrame.pack(side=TOP, fill=BOTH, expand=True)
+        self.currentFrame.pack(side=TOP, fill=BOTH, expand=True, padx = 20, pady= 20)
         
         
         
@@ -56,6 +54,9 @@ class PageOne(Frame):
     fig3 = None
     fig4 = None
     
+    matched_images = []
+    disp_images = []
+
     imgname2 = None
     imgname3 = None
     imgname4 = None
@@ -65,101 +66,77 @@ class PageOne(Frame):
     accuracy4 = None
     
     buttons = None
-    canvas = None
+    lcanvas = None
+    rcanvas = None
     msg = None
-
+    mainframe = None
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         global image_path
         label = ttk.Label(self, text="Ketok Magic Halal", font = LARGE_FONT)
         label.pack(pady = 10, padx = 10)
             
-            # def on_key_press(event):
-            #     print("you pressed {}".format(event.key))
-            #     key_press_handler(event, canvas, toolbar)
-        
-        self.buttons = Frame(self)
-        self.buttons.pack(side = BOTTOM, expand = True, fill = BOTH)
-        
         self.msg = StringVar()
         ttk.Label(self, textvariable=self.msg).pack(side = BOTTOM)
         self.msg.set('Welcome')
-
-        self.canvas = Canvas(self, width=1280, height=600, borderwidth=4)
-        self.canvas.pack(side = TOP, expand = True, fill = BOTH)
         
+        self.buttons = Frame(self)
+        self.buttons.pack(side = BOTTOM, pady = 20)
+        
+        self.mainframe = Frame(self)
+        self.mainframe.pack(side = TOP, expand = True, fill = BOTH);
+
+        self.lcanvas = Canvas(self.mainframe, borderwidth=4, relief = GROOVE, height = 360, width = 360)
+        self.lcanvas.pack(side = LEFT)
+        
+        self.rcanvas = Canvas(self.mainframe, borderwidth=4, relief = GROOVE)
+        self.rcanvas.pack(side = RIGHT, expand = True, fill = BOTH)
+
         button0 = ttk.Button(self.buttons, text="Pilih Gambar", command = lambda: self.SelectImage(controller))
         button0.grid(row = 2, column = 0, padx = (20, 20), sticky = "E")
 
-        button1 = ttk.Button(self.buttons, text="Update", command = lambda: self.draw_image())
-        button1.grid(row = 2, column = 1, padx = (20, 20), sticky = "W")
         
-        button2 = ttk.Button(self.buttons, text="Match", command = lambda: self.matchs())
+        button2 = ttk.Button(self.buttons, text="Match (CS)", command = lambda: Thread(target = self.matchs, args = (True,)).start())
+        button2.grid(row = 2, column = 1, padx = (20, 20), sticky = "W")
+        
+        button2 = ttk.Button(self.buttons, text="Match (ED)", command = lambda: Thread(target = self.matchs, args = (False,)).start())
         button2.grid(row = 2, column = 2, padx = (20, 20), sticky = "W")
+
 
     def draw_image(self):
         global image_path
-        #img = mpimg.imread(image_path)
-        print(image_path)
 
-        # Subplot 1st row 2nd col
-        # self.sub1 = fig.add_subplot(2, 3, 2)
-        # plt.imshow(img)
-        self.sub1 = ImageTk.PhotoImage(Image.open(image_path).resize((540, 540), Image.ANTIALIAS))
+        self.sub1 = ImageTk.PhotoImage(Image.open(image_path).resize((360, 360), Image.ANTIALIAS))
         if (self.fig1 is None):
-            self.fig1 = self.canvas.create_image(20, 20, anchor = NW, image = self.sub1)
+            self.fig1 = self.lcanvas.create_image(0, 0, anchor = NW, image = self.sub1)
         else:
-            self.canvas.itemconfig(self.fig1, image = self.sub1)
-        # Subplot 2nd row 1st col
-        # self.sub2 = fig.add_subplot(2, 3, 4)
-        # plt.imshow(img)
-        self.sub2 = ImageTk.PhotoImage(Image.open(image_path).resize((160, 160), Image.ANTIALIAS))
-        if (self.fig2 is None):
-            self.fig2 = self.canvas.create_image(580, 40, anchor = NW, image = self.sub2)
-            self.imgname2 = self.canvas.create_text(820, 60, text = "Titit gede")
-            self.accuracy2 = self.canvas.create_text(820, 90, text = "Gede banget ahhhh")
-        else:
-            self.canvas.itemconfig(self.fig2, image = self.sub2)
+            self.lcanvas.itemconfig(self.fig1, image = self.sub1)
 
-        # Subplot 2nd row 2nd col
-        # self.sub3 = fig.add_subplot(2, 3, 5)
-        # plt.imshow(img)
-        self.sub3 = ImageTk.PhotoImage(Image.open(image_path).resize((160, 160), Image.ANTIALIAS))
-        if (self.fig3 is None):
-            self.fig3 = self.canvas.create_image(580, 220, anchor = NW, image = self.sub3)
-            self.imgname3 = self.canvas.create_text(820, 240, text = "Titit gede")
-            self.accuracy3 = self.canvas.create_text(820, 270, text = "Gede banget ahhhh")
-        else:
-            self.canvas.itemconfig(self.fig3, image = self.sub3)
-        
+    def draw_matches(self):
+        global t_compare
+        for i in range(t_compare):
+            if (i >= len(self.disp_images)):
+                self.disp_images.append(dict())
+            self.disp_images[i]['img'] = ImageTk.PhotoImage(Image.open(self.matched_images[i]['path']).resize((100, 100), Image.ANTIALIAS))
+            if not ('fig' in self.disp_images[i].keys()):
+                self.disp_images[i]['fig'] = self.rcanvas.create_image(20, 20 + 120 * i, anchor = NW, image = self.disp_images[i]['img'])
+                self.disp_images[i]['str'] = '#' + str(i + 1) + '\nPath : ' + self.matched_images[i]['path'] + '\nSimilarity : ' + str(self.matched_images[i]['x'])
+                self.disp_images[i]['text'] = self.rcanvas.create_text(140, 20 + 120 * i, anchor = NW, text = self.disp_images[i]['str'])
+            else:
+                self.rcanvas.itemconfig(self.disp_images[i]['fig'], image = self.disp_images[i]['img'])
+                self.disp_images[i]['str'] = '#' + str(i + 1) + '\nPath : ' + self.matched_images[i]['path'] + '\nSimilarity : ' + str(self.matched_images[i]['x'])
+                self.rcanvas.itemconfig(self.disp_images[i]['text'], text = self.disp_images[i]['str'])
 
-        # Subplot 2nd row 3rd col
-        # self.sub4 = fig.add_subplot(2, 3, 6)
-        # plt.imshow(img)
-        self.sub4 = ImageTk.PhotoImage(Image.open(image_path).resize((160, 160), Image.ANTIALIAS))
-        if (self.fig4 is None):
-            self.fig4 = self.canvas.create_image(580, 400, anchor = NW, image = self.sub4)
-            self.imgname4 = self.canvas.create_text(820, 420, text = "Titit gede")
-            self.accuracy4 = self.canvas.create_text(820, 450, text = "Gede banget ahhhh")
-        else:
-            self.canvas.itemconfig(self.fig4, image = self.sub4)
-        
-
-        #canvas = FigureCanvasTkAgg(fig, self)  # A tk.DrawingArea.
-        #canvas.draw()
-        #canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-        
-        # toolbar = NavigationToolbar2Tk(canvas, window)
-        # toolbar.update()
-        #canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1) 
     def SelectImage(self, controller):
         global image_path
         upload_img = filedialog.askopenfilename(initialdir = "./", title = "Select file", filetypes =  (("jpeg files","*.jpg"),("all files","*.*")))
-        image_path = upload_img
-        #controller.show_frame(PageOne)
-        self.draw_image()
-        self.msg.set('Opened %s.' %image_path.split('/')[-1])
-        print(image_path)
+        print(type(upload_img), len(upload_img))
+        if type(upload_img) == str:
+            if len(upload_img) > 0:
+                image_path = upload_img
+                print(image_path)
+                self.draw_image()
+                self.msg.set('Opened %s.' %image_path.split('/')[-1])
 
     def load_db(self, path):
         global db
@@ -169,26 +146,37 @@ class PageOne(Frame):
         else:
             self.msg.set('pins.db not found. Please run generate-pickle-file.py to generate it.')
 
-    def matchs(self):
+    def matchs(self , use_cs):
         global db
         global image_path
-        #self.buttons.pack_forget()
-        self.msg.set('Extracting %s...' %image_path.split('/')[-1])
-        sim = 0
-        name = ' '
-        dsc = extractFeatures(image_path)
-        for e in db:
-            print('\rMatching: %s' %e, end='')
-            k = db[e]
-            x = calcCosineSimilarity(dsc, k)
-            if x > sim:
-                sim = x
-                name = e
-        #self.buttons.pack()
-        self.msg.set('Match with: ' + name + ' (' + str(sim) + ') ')
+        if image_path is not None:
+            self.buttons.pack_forget()
+            self.msg.set('Extracting %s...' %image_path.split('/')[-1])
+            self.matched_images = []
+            dsc = extractFeatures(image_path)
+            for compared_path in db:
+                self.msg.set('\rMatching: %s' %compared_path)
+                compared_data = dict()
+                compared_data['path'] = compared_path
+                compared_image = db[compared_path]
+                if use_cs:
+                    compared_data['x'] = calcCosineSimilarity(dsc, compared_image)
+                else:
+                    compared_data['x'] = calcEuclideanDistance(dsc, compared_image)
+                self.matched_images.append(compared_data)
+                self.matched_images = sorted(self.matched_images, key = lambda x: -x['x'])
+                if len(self.matched_images) > t_compare:
+                    self.matched_images = self.matched_images[0:t_compare]
+        
+            self.buttons.pack(side = BOTTOM, pady = 20)
+            self.draw_matches()
+            self.msg.set('Match with: ' + self.matched_images[0]['path'] + ' (' + str(self.matched_images[0]['x']) + ') ')
+        else:
+            self.msg.set('Open a file first.')
+
 if __name__ == '__main__':        
     app = MainApp()
-    app.geometry("1280x720")
+    app.geometry("800x600")
     app.mainloop()
         
         
